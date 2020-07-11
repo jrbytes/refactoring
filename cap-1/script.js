626ca4f00288f3e838a8a7cf30c062491c81fdb5
Jr Bytes <itamazor@gmail.com>
2020-07-11 15:48:46 -0300

    Inline variable. totalAmount and totalVolumeCredits. Renamed variables.

--- cap-1/script.js
+++ cap-1/script.js
@@ -45,19 +45,19 @@ function usd(aNumber) {
 }
 
 function totalVolumeCredits() {
-  let volumeCredits = 0
+  let result = 0
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf)
+    result += volumeCreditsFor(perf)
   }
-  return volumeCredits
+  return result
 }
 
-function appleSauce() {
-  let totalAmount = 0
+function totalAmount() {
+  let result = 0
   for (let perf of invoice.performances) {
-    totalAmount += amountFor(perf)
+    result += amountFor(perf)
   }
-  return totalAmount
+  return result
 }
 
 function statement (invoice, plays) {
@@ -65,9 +65,7 @@ function statement (invoice, plays) {
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
   }
-  let totalAmount = appleSauce()
-
-  result += `Amount owed is ${usd(totalAmount)}\n`
+  result += `Amount owed is ${usd(totalAmount())}\n`
   result += `You earned ${totalVolumeCredits()} credits\n`
   return result
 }
