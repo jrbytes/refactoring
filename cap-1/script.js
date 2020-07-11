dcb19db3f0d0a662cde1c1981b2cd063e10e0e04
Jr Bytes <itamazor@gmail.com>
2020-07-11 15:45:36 -0300

    Extract function appleSauce.

--- cap-1/script.js
+++ cap-1/script.js
@@ -52,14 +52,20 @@ function totalVolumeCredits() {
   return volumeCredits
 }
 
-function statement (invoice, plays) {
+function appleSauce() {
   let totalAmount = 0
-  let result = `Statement for ${invoice.customer}\n`
-
   for (let perf of invoice.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
     totalAmount += amountFor(perf)
   }
+  return totalAmount
+}
+
+function statement (invoice, plays) {
+  let result = `Statement for ${invoice.customer}\n`
+  for (let perf of invoice.performances) {
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
+  }
+  let totalAmount = appleSauce()
 
   result += `Amount owed is ${usd(totalAmount)}\n`
   result += `You earned ${totalVolumeCredits()} credits\n`
