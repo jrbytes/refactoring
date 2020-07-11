f6fa4cebf77fa5f0c051ed324bc77c3c04dc338f
Jr Bytes <itamazor@gmail.com>
2020-07-11 12:34:05 -0300

    Change function declaration. amountFor

--- cap-1/script.js
+++ cap-1/script.js
@@ -3,10 +3,10 @@ const plays = require('./plays')
 
 console.log(statement(invoice, plays))
 
-function amountFor(aPerformance, play) {
+function amountFor(aPerformance) {
   let result = 0
 
-  switch (play.type) {
+  switch (playFor(aPerformance).type) {
     case 'tragedy':
       result = 40000
       if (aPerformance.audience > 30) {
@@ -22,7 +22,7 @@ function amountFor(aPerformance, play) {
       break
   
     default:
-      throw new Error(`unknown type: ${play.type}`)
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`)
   }
   return result
 }
@@ -40,7 +40,7 @@ function statement (invoice, plays) {
   }).format
   
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf))
+    let thisAmount = amountFor(perf)
 
   volumeCredits += Math.max(perf.audience - 30, 0)
 
