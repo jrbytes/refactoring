f6ef51fa335de3de8da00a93da50275552fdc51f
Jr Bytes <itamazor@gmail.com>
2020-07-11 12:40:07 -0300

    Inline variable. statement

--- cap-1/script.js
+++ cap-1/script.js
@@ -46,8 +46,8 @@ function statement (invoice, plays) {
 
   if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
 
-  result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`
-  totalAmount += thisAmount
+  result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`
+  totalAmount += amountFor(perf)
   }
 
   result += `Amount owed is ${format(totalAmount/100)}\n`
