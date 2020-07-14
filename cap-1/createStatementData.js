ee73927f41d09a56f1f12f6fbe57ad9b2a33e0f6
Jr Bytes <itamazor@gmail.com>
2020-07-14 12:14:12 -0300

    Replace conditional with polymorphism. volumeCredits

--- cap-1/createStatementData.js
+++ cap-1/createStatementData.js
@@ -5,10 +5,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0
-    result += Math.max(this.performance.audience - 30, 0)
-    if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5)
-    return result
+    return Math.max(this.performance.audience - 30, 0)
   }
 }
 
@@ -32,6 +29,10 @@ class TragedyCalculator extends PerformanceCalculator {
 }
 
 class ComedyCalculator extends PerformanceCalculator {
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5)
+  }
+  
   get amount() {
     let result = 30000
     if (this.performance.audience > 20) {
