1362bd4cbf83834f7f2288bd87578193e78ba177
Jr Bytes <itamazor@gmail.com>
2020-07-14 13:03:47 -0300

    Moved functions and classes.

--- cap-1/createStatementData.js
+++ cap-1/createStatementData.js
@@ -1,48 +1,3 @@
-class PerformanceCalculator {
-  constructor(aPerformance, aPlay) {
-    this.performance = aPerformance
-    this.play = aPlay
-  }
-
-  get volumeCredits() {
-    return Math.max(this.performance.audience - 30, 0)
-  }
-}
-
-function createPerformanceCalculator(aPerformance, aPlay) {
-  switch(aPlay.type) {
-    case 'tragedy': return new TragedyCalculator(aPerformance, aPlay)
-    case 'comedy': return new ComedyCalculator(aPerformance, aPlay)
-    default:
-      throw new Error(`unknow type: ${aPlay.type}`)
-  }
-}
-
-class TragedyCalculator extends PerformanceCalculator {
-  get amount() {
-    let result = 40000
-    if (this.performance.audience > 30) {
-      result += 1000 * (this.performance.audience - 30)
-    }
-    return result
-  }
-}
-
-class ComedyCalculator extends PerformanceCalculator {
-  get volumeCredits() {
-    return super.volumeCredits + Math.floor(this.performance.audience / 5)
-  }
-  
-  get amount() {
-    let result = 30000
-    if (this.performance.audience > 20) {
-      result += 10000 + 500 * (this.performance.audience - 20)
-    }
-    result += 300 * this.performance.audience
-    return result
-  }
-}
-
 export default function createStatementData(invoice, plays) {
   const result = {}
   result.customer = invoice.customer
@@ -72,5 +27,54 @@ export default function createStatementData(invoice, plays) {
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
+  }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch(aPlay.type) {
+    case 'tragedy': return new TragedyCalculator(aPerformance, aPlay)
+    case 'comedy': return new ComedyCalculator(aPerformance, aPlay)
+    default:
+      throw new Error(`unknow type: ${aPlay.type}`)
+  }
+}
+
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance
+    this.play = aPlay
+  }
+
+  get amount() {
+    throw new Error('subclass responsability')
+  }
+
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0)
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30)
+    }
+    return result
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5)
+  }
+  
+  get amount() {
+    let result = 30000
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20)
+    }
+    result += 300 * this.performance.audience
+    return result
   }
 }
